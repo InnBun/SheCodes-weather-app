37c8543d5e4c84ebf2438eacc7ea6e66fba0e038
InnBun <bunechkoinna@gmail.com>
2022-07-16 22:49:15 +0300

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "103f2ec0bf7a290e3c87ff35ae801e7b";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayTemperature);
